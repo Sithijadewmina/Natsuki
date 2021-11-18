b321e49a3afbc8cf5d260c19786665d906e215b1
I don't know <92771213+Sithijadewmina@users.noreply.github.com>
2021-11-18 12:32:30 +0530

    Update MusicPlay.py

--- Natsuki/modules/MusicPlay.py
+++ Natsuki/modules/MusicPlay.py
@@ -5,19 +5,19 @@ __help__ = """
 1) Make bot admin (Group and in channel if use cplay)
 2) Start a voice chat
 3) Try /play [song name] for the first time by an admin
-4) If userbot joined enjoy music, If not add @NatsukiHelper to your group and retry
-* ✨For Channel Music Play✨ *
-1) Make @TheNatsukiBot admin of your channel 
+4) If userbot joined enjoy music, If not add assistant to your group and retry
+* 🌺For Channel Music Play🌺 *
+1) Make @TheAlexiabot admin of your channel 
 2) Send /userbotjoinchannel in linked group
 3) Now send commands in linked group 
-* ✨Groups Music Play✨ *
+* 🌺Groups Music Play🌺 *
 • /play: Play song using youtube music
 • /play [yt url] : Play the given yt url
 • /play [reply yo audio]: Play replied audio
 • /dplay: Play song via deezer
 • /splay: Play song via jio saavn
  
- * ✨Playback✨ *
+ * 🌺Playback🌺 *
 • /player: Open Settings menu of player
 • /skip: Skips the current track
 • /pause: Pause track
@@ -26,7 +26,7 @@ __help__ = """
 • /current: Shows the current Playing track
 • /playlist: Shows playlist
 Player cmd and all other cmds except /play, /current  and /playlist  are only for admins of the group. 
-* ✨Channel Music Play✨ *
+* 🌺Channel Music Play🌺 *
 • /cplay [song name] - play song you requested
 • /cdplay [song name] - play song you requested via deezer
 • /csplay [song name] - play song you requested via jio saavn
@@ -44,16 +44,16 @@ channel is also can be used instead of c ( /cplay = /channelplay )
 1) Get your channel ID.
 2) Create a group with tittle: Channel Music: your_channel_id
 3) Add bot as Channel admin with full perms
-4) Add @TheNatsukiBot to the channel as an admin.
+4) Add @TheAlexiabot to the channel as an admin.
 5) Simply send commands in your group.
  
-* ✨More tools✨ *
+* 🌺More tools🌺 *
 • /admincache: Updates admin info of your group. Try if bot isn't recognize admin
-• /userbotjoin: Invite @NatsukiHelper Userbot to your chat 
+• /userbotjoin: Invite assistant Userbot to your chat 
  
 * 🙋‍♀️Assisten🙋‍♀️ *
-@NatsukiHelper
+Coming soon
 * 🙋‍ Bot 🙋‍♀️ *
-@TheNatsukiBot
+@TheAlexiabot
 """
-__mod_name__ = "Music Play"
+__mod_name__ = "🎧Music Play"
